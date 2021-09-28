526f4362ef6dc29e54d2b5cc3a856521d54afe8f
glebo <glebololo1984@mail.ru>
2021-09-28 22:36:35 +0300

    remake main

--- src/ru/mirea/task2/Main.java
+++ src/ru/mirea/task2/Main.java
@@ -6,32 +6,39 @@ public class Main {
         Shape shape = new Shape(11);
         System.out.println(shape);
 
+
+
+
         System.out.println("\nTesting class Ball");
-        Ball ball1 = new Ball(5, "yellow");
-        Ball ball2 = new Ball(7.5);
-        ball2.setColor("red");
+        Ball ball1 = new Ball(2, "blue");
+        Ball ball2 = new Ball(3);
+        ball2.setColor("yellow");
         System.out.println(ball1);
         System.out.println(ball2);
 
+
+
         System.out.println("\nTesting class Book");
-        book book1 = new book("", "Lev Tolstoy", 1869);
+        book book1 = new book("Voina i mir", "Pushkin", 1800);
         book book2 = new book("Oblomov", "Ivan Goncharov");
-        book book3 = new book("Fathers and Sons");
+        book book3 = new book("Remeslo");
         System.out.println(book1);
         System.out.println(book2);
         System.out.println(book3);
         book1.setName("War and Peace");
-        book2.setPublishingYear(1857);
+        book2.setPublishingYear(1354);
         book3.setAuthor("Ivan Turgenev");
-        book3.setPublishingYear(1861);
+        book3.setPublishingYear(1123);
         System.out.println(book1);
         System.out.println(book2);
         System.out.println(book3);
 
+
+
         System.out.println("\nTesting class TestDog");
         TestDog dogs = new TestDog();
-        Dog dog1 = new Dog("Jack", 10);
-        Dog dog2 = new Dog("Bob", 2);
+        Dog dog1 = new Dog("Flex", 5);
+        Dog dog2 = new Dog("Rave", 8);
         dogs.addDog(dog1);
         dogs.addDog(dog2);
         System.out.println(dogs);
